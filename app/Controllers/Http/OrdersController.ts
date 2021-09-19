cfd367f94f8f130de378a26a4b19e3f9bce6fc6b
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-09-19 14:27:38 +0200

    add validation on updateOrderSchema

--- app/Controllers/Http/OrdersController.ts
+++ app/Controllers/Http/OrdersController.ts
@@ -1,6 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import Order from 'App/Models/Order'
+import { trim } from 'lodash'
 
 export default class OrdersController {
   public async index({ view }: HttpContextContract) {
@@ -14,19 +15,25 @@ export default class OrdersController {
   }
 
   public async update({ request, response, params, session }: HttpContextContract) {
-      const orderData = await request.validate({ schema: updateOrderSchema })
-      const order = await Order.findOrFail(params.id)
-      order.customersName = orderData.customersName,
-      order.customersPhone = orderData.customersPhone,
-      order.customersEmail = orderData.customersEmail
-      await order.save()
-      session.flash('messageSucces', 'Your data was saved')
-      return response.redirect().toRoute('OrdersController.index')
+    const orderData = await request.validate({ schema: updateOrderSchema })
+    const order = await Order.findOrFail(params.id)
+    ;(order.customersName = orderData.customersName),
+      (order.customersPhone = orderData.customersPhone),
+      (order.customersEmail = orderData.customersEmail)
+    await order.save()
+    session.flash('messageSucces', 'Your data was saved')
+    return response.redirect().toRoute('OrdersController.index')
   }
 }
 
 const updateOrderSchema = schema.create({
-  customersName: schema.string(),
-  customersPhone: schema.string(),
-  customersEmail: schema.string(),
+  customersName: schema.string({ escape: true, trim: true }, [rules.minLength(3)]),
+  customersPhone: schema.string({}, [rules.mobile({ strict: true })]),
+  customersEmail: schema.string({}, [
+    rules.email({
+      sanitize: true,
+      ignoreMaxLength: true,
+      domainSpecificValidation: true,
+    }),
+  ]),
 })
